6e6fd302c2046b646921419c536c9ff5ef163f4c
zhangyunfei <zhangyunfei@aerozhonghuan.com>
2021-06-25 23:16:45 +0800

    mybatisdemo2

---
+++ mybatisdemo2/src/main/java/cn/zyfvir/CountryMapper.java
@@ -0,0 +1,10 @@
+package cn.zyfvir;
+
+/**
+ * @description:
+ * @author: zhangyunfei
+ * @date: 2021/6/25 23:06
+ */
+public interface CountryMapper {
+    Country selectBlog(int id);
+}
---
+++ mybatisdemo2/src/main/java/cn/zyfvir/MainClass.java
@@ -0,0 +1,36 @@
+package cn.zyfvir;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+
+/**
+ * @description:
+ * @author: zhangyunfei
+ * @date: 2021/6/25 21:40
+ */
+public class MainClass {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("ready...");
+
+        String resource = "mapper/mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            CountryMapper mapper = session.getMapper(CountryMapper.class);
+            Country blog = mapper.selectBlog(1);
+            printf(blog);
+        }
+    }
+
+    private static void printf(Country blog) {
+        System.out.println("" + blog.id + ", " + blog.countryName);
+    }
+}
